6bc68e7561dd254c50011ccc4f0411fba116e720
Vishal Dhakal <vishaldhakal@Vishals-Air.domain.name>
2020-09-11 18:15:53 +0545

    sdcsd

--- js/app.js
+++ js/app.js
@@ -7,10 +7,13 @@ const batcont = document.querySelector('.bat');
 const bowlcont = document.querySelector('.bowl');
 const allroundcont = document.querySelector('.allround');
 const wicketcont = document.querySelector('.wicket');
+const selectt = document.querySelector('.selec');
+const check = document.querySelectorAll('.css-checkbox');
 
 bowlcont.style.display="none";
 allroundcont.style.display="none";
 wicketcont.style.display="none";
+
 
 bat.addEventListener('click',(e)=>{
     batcont.style.display="block";
@@ -21,6 +24,7 @@ bat.addEventListener('click',(e)=>{
     bowlcont.style.display="none";
     allroundcont.style.display="none";
     wicketcont.style.display="none";
+    selectt.innerHTML = "Select 4-5 Batsman";
 });
 bowl.addEventListener('click',(e)=>{
     batcont.style.display="none";
@@ -31,6 +35,7 @@ bowl.addEventListener('click',(e)=>{
     bowl.className +=" bottom";
     allroundcont.style.display="none";
     wicketcont.style.display="none";
+    selectt.innerHTML = "Select 4-5 Bowler";
 });
 allround.addEventListener('click',(e)=>{
     bowlcont.style.display="none";
@@ -41,6 +46,7 @@ allround.addEventListener('click',(e)=>{
     allround.className +=" bottom";
     allroundcont.style.display="block";
     wicketcont.style.display="none";
+    selectt.innerHTML = "Select 4-5 All Rounders";
 });
 wicket.addEventListener('click',(e)=>{
     bowlcont.style.display="none";
@@ -51,4 +57,22 @@ wicket.addEventListener('click',(e)=>{
     wicket.className +=" bottom";
     batcont.style.display="none";
     wicketcont.style.display="block";
+    selectt.innerHTML = "Select 1 Wicket Keeper";
+});
+
+check.forEach(ele => {
+    ele.addEventListener('click',(e)=>{   
+        const lis = e.target.parentElement.parentElement.classList;
+        let check = 1;
+        lis.forEach(elements => {
+            if(elements == "bottom2"){
+                check = 0;
+            }
+        });
+        if(check == 1){
+            e.target.parentElement.parentElement.className += " bottom2";
+        }else{
+            e.target.parentElement.parentElement.classList.remove("bottom2");
+        }
+    });
 });
