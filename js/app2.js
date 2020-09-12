dc805e3cc6aec4bb7c838de4194db5ca61c3830e
Vishal Dhakal <vishaldhakal@Vishals-MacBook-Air.local>
2020-09-12 22:57:59 +0545

    sdas

--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,15 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('service worker registered'))
-      .catch(err => console.log('service worker not registered', err));
+window.addEventListener('load', e => {
+    new MeroTeam();
+    registerSW(); 
+  });
+  async function registerSW() { 
+    if ('serviceWorker' in navigator) { 
+      try {
+        await navigator.serviceWorker.register('./sw.js'); 
+      } catch (e) {
+        alert('ServiceWorker registration failed. Sorry about that.'); 
+      }
+    } else {
+      document.querySelector('.alert').removeAttribute('hidden'); 
+    }
   }
--- sw.js
+++ sw.js
@@ -1,25 +1,18 @@
-const dynamicCacheName = 'site-dynamic-v1';
-// activate event
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key =>  key !== dynamicCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-// fetch event
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          return fetchRes;
-        })
-      });
-    })
-  );
-});
+self.addEventListener('install', async event => {
+    console.log('install event')
+  });
+  
+  self.addEventListener('fetch', async event => {
+    console.log('fetch event')
+  });
+  const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './js/app.js',
+  './css/styles.css'
+];
+self.addEventListener('install', async event => {
+    const cache = await caches.open(cacheName); 
+    await cache.addAll(staticAssets); 
+  });
